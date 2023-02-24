1498cba8c0ce66963adf0f91be4ff38deee50317
minapark <icherom@naver.com>
2023-02-24 09:07:07 +0900

    two

--- mina/1회차/str_bomb.py
+++ mina/1회차/str_bomb.py
@@ -12,23 +12,24 @@ stack = []
 
 i = 0
 
-while i < len(str_v):
+while i < len(str_v): # str_v의 길이보다 작아야 한다. 0 -> N-1
 
-    stack.append(str_v[i])
+    stack.append(str_v[i]) # stack에 넣는다.
 
-    if i >= length - 1 and bomb[-1] == stack[-1]:
+    # 마지막 글자가 들어올때마다 확인해준다.
+    if i >= length - 1 and bomb[-1] == stack[-1]: #마지막 원소와 같은 값이 들어왔을 때 그 전을 확인
         for j in range(1, length + 1):
-            if bomb[-j] != stack[-j]:
+            if bomb[-j] != stack[-j]: # 혹시 폭탄과 같은가?
                 break
         else:
-            for _ in range(length):
+            for _ in range(length): # 길이만큼 stack에서 빼냄
                 stack.pop()
 
-    i += 1
+    i += 1 # 다음 원소로 이동해서 계속
 
-if not stack:
+if not stack: # 스택이 비어잇으면 FRUA를 출력하고
     print('FRULA')
-else:
+else: # 아니면 그냥 stack 문자열 출력
     print(''.join(stack))
 
 # while로 str_v을 돌면서 stack에 집어넣음
