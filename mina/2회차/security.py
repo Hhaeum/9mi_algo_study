5beee560e7495427e9747db105b0f9292a7dde3a
minapark <icherom@naver.com>
2023-03-10 11:13:13 +0900

    update

--- mina/2회차/security.py
+++ mina/2회차/security.py
@@ -5,25 +5,16 @@ W, H = map(int, input().split())
 N = int(input())
 #  1은 블록의 북쪽, 2는 블록의 남쪽, 3은 블록의 서쪽, 4는 블록의 동쪽에 상점이 있음을 의미한다.
 store = [0]*N
-plus = [0, 0, H, 0, W]
+plus = [0, -2 * (H + W) , H, 0, -(2 * H + W)]
 for i in range(N):
     d, p = map(int, input().split())
-    store[i] = (d, p + plus[d])
+    store[i] = abs(p + plus[d])
 
 # 동근이 위치n
 n, pos = map(int, input().split())
 pos += plus[n]
-length = (W+H)*2
+length = (W + H)*2
 sum_d = 0
-for d, pos_s in store:
-    dn = abs(n - d)
-    min_v = 0
-    if dn == 1 and (n + d) % 5:
-        min_v = min(length - (pos + pos_s), pos + pos_s)
-    elif dn == 2:
-        min_v = min(length - (pos + pos_s), pos + pos_s)
-    else:
-        min_v = abs(pos - pos_s)
-    sum_d += min_v
-
+for pos_s in store:
+    sum_d += min (abs(pos_s - pos), length - abs(pos_s - pos))
 print(sum_d)
